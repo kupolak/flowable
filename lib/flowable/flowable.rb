e2837cb67514be475693e915aaf56a46baae36bb
Jakub Polak <jakub.polak.vz@gmail.com>
2025-07-08 19:07:05 +0100

    Add Client class initialization

--- lib/flowable/flowable.rb
+++ lib/flowable/flowable.rb
@@ -17,4 +17,24 @@ module Flowable
   class ConflictError < Error; end
 
   class Client
+    CMMN_BASE_PATH = '/flowable-rest/cmmn-api'
+    BPMN_BASE_PATH = '/flowable-rest/service'
+
+    attr_reader :host
+    attr_reader :port
+    attr_reader :username
+    attr_reader :base_path
+    attr_reader :bpmn_base_path
+
+    def initialize(host: 'localhost', port: 8080, username:, password:, base_path: CMMN_BASE_PATH,
+                   bpmn_base_path: BPMN_BASE_PATH, use_ssl: false)
+      @host = host
+      @port = port
+      @username = username
+      @password = password
+      @base_path = base_path
+      @bpmn_base_path = bpmn_base_path
+      @use_ssl = use_ssl
+    end
+
 end
