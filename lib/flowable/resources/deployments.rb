e4872fdeb031292cacb2fb7a842beb00225516fd
Jakub Polak <jakub.polak.vz@gmail.com>
2025-07-31 16:38:32 +0100

    Add Deployments methods (part 2)

--- lib/flowable/resources/deployments.rb
+++ lib/flowable/resources/deployments.rb
@@ -6,3 +6,11 @@ module Flowable
       BASE_PATH = 'cmmn-repository/deployments'
 
       # List all deployments
+      # @param options [Hash] Query parameters
+      # @option options [String] :name Filter by exact name
+      # @option options [String] :nameLike Filter by name pattern (use % wildcard)
+      # @option options [String] :category Filter by category
+      # @option options [String] :tenantId Filter by tenant
+      # @option options [Integer] :start Pagination start (default: 0)
+      # @option options [Integer] :size Page size (default: 10)
+      # @option options [String] :sort Sort field (id/name/deployTime/tenantId)
