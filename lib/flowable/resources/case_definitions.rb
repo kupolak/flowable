f19c4dbee84562ecd821dc468c270a5149ef5b77
Jakub Polak <jakub.polak.vz@gmail.com>
2025-08-13 18:01:17 +0100

    Add Case Definitions methods (part 2)

--- lib/flowable/resources/case_definitions.rb
+++ lib/flowable/resources/case_definitions.rb
@@ -7,3 +7,12 @@ module Flowable
 
       # List all case definitions
       # @param options [Hash] Query parameters
+      # @option options [String] :key Filter by key
+      # @option options [String] :keyLike Filter by key pattern
+      # @option options [String] :name Filter by name
+      # @option options [String] :nameLike Filter by name pattern
+      # @option options [Integer] :version Filter by version
+      # @option options [String] :deploymentId Filter by deployment
+      # @option options [Boolean] :latest Only return latest versions
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of case definitions
