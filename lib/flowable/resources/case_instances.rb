f601d3d0abf2a4c9890fb437cc0f9af3cbcf66c5
Jakub Polak <jakub.polak.vz@gmail.com>
2025-08-19 12:47:46 +0100

    Add Case Instances methods (part 2)

--- lib/flowable/resources/case_instances.rb
+++ lib/flowable/resources/case_instances.rb
@@ -12,3 +12,17 @@ module Flowable
       # @option options [String] :caseDefinitionId Filter by definition ID
       # @option options [String] :businessKey Filter by business key
       # @option options [String] :involvedUser Filter by involved user
+      # @option options [Boolean] :includeCaseVariables Include variables in response
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of case instances
+      def list(**options)
+        params = paginate_params(options)
+        %i[id caseDefinitionKey caseDefinitionId businessKey
+           involvedUser tenantId tenantIdLike].each do |key|
+          params[key] = options[key] if options[key]
+        end
+        params[:includeCaseVariables] = options[:includeCaseVariables] if options.key?(:includeCaseVariables)
+        params[:withoutTenantId] = options[:withoutTenantId] if options.key?(:withoutTenantId)
+
+        client.get(BASE_PATH, params)
+      end
