263c05a5ad8be8131d49ddc6209c3f7e7458b4bd
Jakub Polak <jakub.polak.vz@gmail.com>
2025-09-08 18:49:35 +0100

    Add Plan Item Instances methods (part 2)

--- lib/flowable/resources/plan_item_instances.rb
+++ lib/flowable/resources/plan_item_instances.rb
@@ -8,3 +8,13 @@ module Flowable
       # List all plan item instances
       # @param options [Hash] Query parameters
       # @option options [String] :id Filter by ID
+      # @option options [String] :caseDefinitionId Filter by case definition
+      # @option options [String] :caseInstanceId Filter by case instance
+      # @option options [String] :stageInstanceId Filter by parent stage
+      # @option options [String] :planItemDefinitionId Filter by plan item definition
+      # @option options [String] :planItemDefinitionType Filter by type (stage, milestone, humanTask, etc.)
+      # @option options [String] :planItemDefinitionTypes Comma-separated types
+      # @option options [String] :state Filter by state (available, active, enabled, disabled, completed, etc.)
+      # @option options [String] :name Filter by name
+      # @option options [String] :elementId Filter by element ID from model
+      # @option options [String] :tenantId Filter by tenant
