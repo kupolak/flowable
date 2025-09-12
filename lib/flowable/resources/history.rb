87211caf2d767bc7ad39201a6d1166783f8f3fef
Jakub Polak <jakub.polak.vz@gmail.com>
2025-09-12 17:53:52 +0100

    Add History methods (part 2)

--- lib/flowable/resources/history.rb
+++ lib/flowable/resources/history.rb
@@ -18,3 +18,23 @@ module Flowable
       # @option options [String] :startedAfter Started after date
       # @option options [String] :startedBefore Started before date
       # @option options [String] :startedBy Filter by starter user
+      # @option options [Boolean] :includeCaseVariables Include variables
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of historic case instances
+      def case_instances(**options)
+        params = paginate_params(options)
+        %i[caseInstanceId caseDefinitionKey caseDefinitionId businessKey
+           involvedUser startedBy tenantId tenantIdLike].each do |key|
+          params[key] = options[key] if options[key]
+        end
+
+        %i[finished includeCaseVariables withoutTenantId].each do |key|
+          params[key] = options[key] if options.key?(key)
+        end
+
+        %i[finishedAfter finishedBefore startedAfter startedBefore].each do |key|
+          params[key] = format_date(options[key]) if options[key]
+        end
+
+        client.get('cmmn-history/historic-case-instances', params)
+      end
