64d29509d2789d50e251af3381d176fe24ac76da
Jakub Polak <jakub.polak.vz@gmail.com>
2025-09-20 11:18:13 +0100

    Extend Bpmn Deployments (2/15)

--- lib/flowable/resources/bpmn_deployments.rb
+++ lib/flowable/resources/bpmn_deployments.rb
@@ -5,3 +5,10 @@ module Flowable
     class BpmnDeployments < Base
       BASE_PATH = 'service/repository/deployments'
 
+      # List all BPMN deployments
+      # @param options [Hash] Query parameters
+      # @option options [String] :name Filter by exact name
+      # @option options [String] :nameLike Filter by name pattern (use % wildcard)
+      # @option options [String] :category Filter by category
+      # @option options [String] :tenantId Filter by tenant
+      # @option options [Integer] :start Pagination start (default: 0)
