b54b3aa62bbe618d13619308256f49bf0e0a7918
Jakub Polak <jakub.polak.vz@gmail.com>
2025-09-30 20:47:56 +0100

    Extend Process Definitions (2/15)

--- lib/flowable/resources/process_definitions.rb
+++ lib/flowable/resources/process_definitions.rb
@@ -9,3 +9,14 @@ module Flowable
       # @param options [Hash] Query parameters
       # @option options [String] :key Filter by key
       # @option options [String] :keyLike Filter by key pattern
+      # @option options [String] :name Filter by name
+      # @option options [String] :nameLike Filter by name pattern
+      # @option options [Integer] :version Filter by version
+      # @option options [String] :deploymentId Filter by deployment
+      # @option options [Boolean] :latest Only return latest versions
+      # @option options [Boolean] :suspended Filter by suspension state
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of process definitions
+      def list(**options)
+        params = paginate_params(options)
+        %i[key keyLike name nameLike resourceName resourceNameLike
