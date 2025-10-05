593d0abc4f68a8d7f2c306cc9fed960b65b1e2e4
Jakub Polak <jakub.polak.vz@gmail.com>
2025-10-05 10:07:04 +0100

    Extend Process Instances (2/15)

--- lib/flowable/resources/process_instances.rb
+++ lib/flowable/resources/process_instances.rb
@@ -13,3 +13,18 @@ module Flowable
       # @option options [String] :businessKey Filter by business key
       # @option options [String] :involvedUser Filter by involved user
       # @option options [Boolean] :suspended Filter suspended instances
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of process instances
+      def list(**options)
+        params = paginate_params(options)
+        %i[id processDefinitionKey processDefinitionId businessKey
+           involvedUser superProcessInstanceId tenantId tenantIdLike].each do |key|
+          params[key] = options[key] if options[key]
+        end
+        params[:suspended] = options[:suspended] if options.key?(:suspended)
+        params[:withoutTenantId] = options[:withoutTenantId] if options.key?(:withoutTenantId)
+        params[:includeProcessVariables] = options[:includeProcessVariables] if options.key?(:includeProcessVariables)
+
+        client.get(BASE_PATH, params)
+      end
+
