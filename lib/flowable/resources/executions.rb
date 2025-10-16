3d8f5b884ff0f5d405bfed11b6c61c68d675f298
Jakub Polak <jakub.polak.vz@gmail.com>
2025-10-16 14:48:52 +0100

    Extend Executions (2/15)

--- lib/flowable/resources/executions.rb
+++ lib/flowable/resources/executions.rb
@@ -9,3 +9,14 @@ module Flowable
       # @param options [Hash] Query parameters
       # @option options [String] :id Filter by execution ID
       # @option options [String] :processDefinitionKey Filter by process definition key
+      # @option options [String] :processDefinitionId Filter by process definition ID
+      # @option options [String] :processInstanceId Filter by process instance ID
+      # @option options [String] :activityId Filter by activity ID
+      # @option options [String] :parentId Filter by parent execution ID
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of executions
+      def list(**options)
+        params = paginate_params(options)
+        %i[id processDefinitionKey processDefinitionId processInstanceId
+           activityId parentId signalEventSubscriptionName
+           messageEventSubscriptionName tenantId tenantIdLike].each do |key|
