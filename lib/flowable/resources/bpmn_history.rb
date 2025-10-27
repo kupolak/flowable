9a7d4dae99a49b3e2ebc2da6fb1bad14860e113b
Jakub Polak <jakub.polak.vz@gmail.com>
2025-10-27 17:03:01 +0100

    Extend Bpmn History (2/15)

--- lib/flowable/resources/bpmn_history.rb
+++ lib/flowable/resources/bpmn_history.rb
@@ -14,3 +14,19 @@ module Flowable
       # @option options [String] :involvedUser Filter by involved user
       # @option options [Boolean] :finished Only finished instances
       # @option options [Boolean] :includeProcessVariables Include variables
+      # @option options [String] :tenantId Filter by tenant
+      # @return [Hash] Paginated list of historic process instances
+      def process_instances(**options)
+        params = paginate_params(options)
+        %i[processInstanceId processDefinitionKey processDefinitionId businessKey
+           involvedUser superProcessInstanceId startedBy tenantId tenantIdLike].each do |key|
+          params[key] = options[key] if options[key]
+        end
+
+        %i[finished includeProcessVariables withoutTenantId].each do |key|
+          params[key] = options[key] if options.key?(key)
+        end
+
+        %i[finishedAfter finishedBefore startedAfter startedBefore].each do |key|
+          params[key] = format_date(options[key]) if options[key]
+        end
